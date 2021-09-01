f34a190ff271ef93b793a513955816c7471c030b
Erin Holmes <e.holmes09@yahoo.com>
2021-09-01 16:44:21 -0500

    set up comment model, controller, and routes, integrate with front-end

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const { Shema, model, Schema } = require("mongoose");
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Comment = model("Comment", CommentSchema);
+
+module.exports = Comment;
